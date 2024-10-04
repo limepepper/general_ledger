ecdef22bc53db24a48e997d76e96b0b1ab01cde8
Tom Hodder <tom@limepepper.co.uk>
2024-10-04 08:06:16 +0100

    git squash this

---
+++ docs/source/installation.rst
@@ -0,0 +1,23 @@
+.. _installation:
+
+============
+Installation
+============
+
+Install this package using
+
+.. code-block:: shell
+
+	pip install django-general-ledger
+
+
+and add this app to the project's ``settings.py``:
+
+.. code-block:: python
+	:caption: settings.py
+
+	INSTALLED_APPS = [
+	    ...
+	    'general_ledger',
+	    ...
+	]
--- general_ledger/builders/invoice_builder.py
+++ general_ledger/builders/invoice_builder.py
@@ -1,47 +1,63 @@
 from datetime import date
 
-from general_ledger.models import Account, InvoiceLine
+from loguru import logger
+
 from general_ledger.models import (
-    Ledger,
-    Payment,
     Invoice,
-    BankStatementLine,
     DocumentNumberSequence as DocNumSeq,
     Account,
     TaxRate,
 )
-from loguru import logger
+from general_ledger.models import InvoiceLine
 
 
 class InvoiceBuilder:
     def __init__(
         self,
-        ledger=None,
+        *,
+        ledger,
         **kwargs,
     ):
-        self.invoice = None
+        logger.trace(f"init in InvoiceBuilder '{kwargs=}'")
         self.ledger = ledger
+        if not self.ledger:
+            raise ValueError("Ledger is required")
+        self.book = self.ledger.book
+        self.invoice = None
         self.kwargs = kwargs
         self.date = kwargs.get("date", date.today())
         self.entries = []
         self.invoice_lines = []
-        logger.trace(f"init in InvoiceBuilder {kwargs=}")
 
     def add_memo_invoice(
         self,
         **kwargs,
     ):
         logger.info(f"Adding memo invoice {kwargs=}")
+        if not kwargs.get("contact"):
+            raise ValueError("contact is required")
+        contact = kwargs.get("contact")
         self.create_invoice(**kwargs)
         qs = Account.objects.for_book(self.ledger.book)
+        taxrate_qs = TaxRate.objects.for_book(self.ledger.book)
+        if kwargs.get("sales_account__slug"):
+            sales_account = qs.get(slug=kwargs.get("sales_account__slug"))
+        elif hasattr(contact, "sales_account") and contact.sales_account:
+            sales_account = getattr(contact, "sales_account")
+        else:
+            sales_account = self.ledger.coa.get_sales_account()
+        if kwargs.get("tax_rate__slug"):
+            vat_rate = taxrate_qs.get(slug=kwargs.get("tax_rate__slug"))
+        elif hasattr(contact, "sales_tax_rate") and contact.sales_tax_rate:
+            vat_rate = getattr(contact, "sales_tax_rate")
+        else:
+            vat_rate = self.book.taxrate_set.get(slug="20-vat-on-income")
         self.invoice.invoice_lines.create(
             invoice=self.invoice,
             quantity=1,
             unit_price=kwargs.get("amount"),
-            account=qs.get(slug=kwargs["sales_account__slug"]),
-            vat_rate=TaxRate.objects.for_book(self.ledger.book).get(
-                slug=kwargs["tax_rate__slug"]
-            ),
+            account=sales_account,
+            vat_rate=vat_rate,
         )
         return self
 
@@ -49,7 +65,7 @@ class InvoiceBuilder:
         qs = Account.objects.for_ledger(self.ledger)
         sales_account = qs.filter(slug=kwargs.get("sales_account__slug", None)).first()
         self.invoice = Invoice(
-            invoice_number=f"PAY-{DocNumSeq.get_next_number('PAY')}",
+            invoice_number=DocNumSeq.get_next_number('PAY'),
             contact=kwargs.get("contact"),
             date=kwargs.get("date", self.date),
             due_date=kwargs.get("due_date", None),
